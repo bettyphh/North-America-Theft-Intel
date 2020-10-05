48a571b855428fdb83df7165c4616106ef1ab0ec
Betty Ho <betty.ho@over-haul.com>
2020-10-05 11:09:28 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='North America Cargo Theft Data Relationship',
+    author='bettyphh',
+    license='',
+)
